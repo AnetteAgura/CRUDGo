1851d45ef1ffeba2ee0395ffeaecf9687a299762
AnetteAguraiuja <anetteagura@gmail.com>
2022-03-02 00:27:00 +0200

    Fixed database file

--- RUN.sql
+++ RUN.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `games`
 --
-
+CREATE DATABASE games;
+USE games;
 DROP TABLE IF EXISTS `games`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
