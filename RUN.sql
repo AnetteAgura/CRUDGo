e41094314843c4a29a0eed313850cda5a16353c8
AnetteAgura <anetteagura@gmail.com>
2022-03-05 17:52:03 +0200

    Update RUN.sql

--- RUN.sql
+++ RUN.sql
@@ -1,27 +1,7 @@
--- MySQL dump 10.13  Distrib 5.6.51, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: games
--- ------------------------------------------------------
--- Server version	5.6.51-log
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `games`
---
 
 DROP TABLE IF EXISTS `games`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `games` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
@@ -29,25 +9,11 @@ CREATE TABLE `games` (
   `company` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `games`
---
 
 LOCK TABLES `games` WRITE;
-/*!40000 ALTER TABLE `games` DISABLE KEYS */;
+
 INSERT INTO `games` (`id`, `name`, `year`, `company`) VALUES (4,'Elden Ring',2022,'Bandai Namco'),(8,'Division 2',2019,'Ubisoft'),(9,'Rayman Legends',2013,'Ubisoft'),(10,'Dark Souls 3',2016,'Bandai Namco'),(11,'Disney Magical World',2013,'Bandai Namco'),(12,'Assassin\'s Creed Odyssey',2018,'Ubisoft'),(13,'Bloodborne',2015,'FromSoftware'),(14,'The Elder Scrolls Online',2014,'ZeniMax'),(15,'Just Cause 3',2015,'Avalanche'),(16,'Prototype 2',2012,'Radical Entertainment'),(17,'Prototype',2009,'Radical Entertainment'),(18,'Roblox',2006,'Roblox Corporation'),(19,'Ghost of Tsushima',2020,'Sucker Punch Productions');
-/*!40000 ALTER TABLE `games` ENABLE KEYS */;
+
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-03-02  0:03:31
