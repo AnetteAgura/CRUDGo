7b19fc04ea6d47bab544a9e1d105da3d0311f8a7
Henry-Gerret Grüning <Henry-gerret.gruning@khk.ee>
2022-03-05 17:55:59 +0200

    Renamed RUN.SQL to Database.sql

--- database.sql
+++ database.sql
@@ -15,5 +15,5 @@ LOCK TABLES `games` WRITE;
 
 INSERT INTO `games` (`id`, `name`, `year`, `company`) VALUES (4,'Elden Ring',2022,'Bandai Namco'),(8,'Division 2',2019,'Ubisoft'),(9,'Rayman Legends',2013,'Ubisoft'),(10,'Dark Souls 3',2016,'Bandai Namco'),(11,'Disney Magical World',2013,'Bandai Namco'),(12,'Assassin\'s Creed Odyssey',2018,'Ubisoft'),(13,'Bloodborne',2015,'FromSoftware'),(14,'The Elder Scrolls Online',2014,'ZeniMax'),(15,'Just Cause 3',2015,'Avalanche'),(16,'Prototype 2',2012,'Radical Entertainment'),(17,'Prototype',2009,'Radical Entertainment'),(18,'Roblox',2006,'Roblox Corporation'),(19,'Ghost of Tsushima',2020,'Sucker Punch Productions');
 
-UNLOCK TABLES;
+
 
